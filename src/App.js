520d7a9bae03207e60d73aa4832b367cbffeb1fe
pankaj <pankajyadav221306@gmail.com>
2025-11-06 21:25:50 +0530

    hero sec implimentions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Products from "./Pages/Products";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory Category="men" />} />
+          <Route path="/womens" element={<ShopCategory Category="women" />} />
+          <Route path="/kids" element={<ShopCategory Category="kid" />} />
+          <Route path="products" element={<Products />}>
+            <Route path=":productId" element={<Products />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
