619d65edea9fe42aa9babfaff500cf9995fa215a
pankaj <pankajyadav221306@gmail.com>
2025-11-15 08:23:28 +0530

    shopcategory code and page adding

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Products from "./Pages/Products";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
 import Footer from "./Components/Footer/Footer";
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
 
 function App() {
   return (
@@ -15,9 +18,18 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory Category="men" />} />
-          <Route path="/womens" element={<ShopCategory Category="women" />} />
-          <Route path="/kids" element={<ShopCategory Category="kid" />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} Category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} Category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} Category="kid" />}
+          />
           <Route path="products" element={<Products />}>
             <Route path=":productId" element={<Products />} />
           </Route>
