579479f12a7fe45a84dd66dd9d78175b9b19055e
pankaj <pankajyadav221306@gmail.com>
2025-11-21 08:36:58 +0530

    changing product exc

--- src/Pages/Products.jsx
+++
@@ -1,8 +0,0 @@
-// import React, { useContext } from "react";
-
-const Products = () => {
-  // const { all_product } = useContext(ShopContext);
-  return <div></div>;
-};
-
-export default Products;
